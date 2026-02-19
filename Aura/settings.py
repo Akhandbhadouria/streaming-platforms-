db16077b21b963b5f72b6324c27cab9587f31826
mayanksinghrajpoot <mayanksinghrajpoot01@gmail.com>
2026-02-20 03:29:33 +0530

    updating setting.py to use other db else then sqlite3

--- Aura/settings.py
+++ Aura/settings.py
@@ -87,12 +87,19 @@ WSGI_APPLICATION = 'Aura.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default=f'sqlite:///{BASE_DIR / "db.sqlite3"}',
-        conn_max_age=600,
-    )
-}
+if os.getenv('DATABASE_URL'):
+    DATABASES = {
+        'default': dj_database_url.config(
+            conn_max_age=600,
+        )
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
